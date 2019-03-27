fa698735bf9c1013466d608205797ff30a72a5e1
arvinyustin99 <arvinyustin99@gmail.com>
2019-03-28 00:43:31 +0700

    update ADT Priority Queue

---
+++ priorityQueueClass.py
@@ -0,0 +1,45 @@
+class PriorityQueue:
+	#CTOR
+	def __init__(self):
+		#data[i] = [a, b, c]
+		#a = posisi X
+		#b = posisi Y
+		#c = cost
+		self.data = []
+		self.size = 0
+	#SISIP
+	def Insert(self, L):
+		i = 0
+		if (self.IsEmpty != False):
+			while (L[2] > self.data[i][2]):
+				i = i + 1
+			#sudah berada di posisi pas
+			self.data.insert(i, L)
+		else:
+			self.data.append(L)
+		self.size += 1
+
+	#PRINT
+	def Print(self):
+		for i in range(0, self.Count(), 1):
+			print(self.data[i], end = "\n")
+	
+	#PREDIKAT
+	def Count(self):
+		return self.size
+	def IsEmpty(self):
+		return size == 0
+		
+def main():
+	example = PriorityQueue()
+	ins1 = [11, 4, 10]
+	ins2 = [12, 3, 3]
+	ins3 = [4, 5, 6]
+	
+	example.Insert(ins1)
+	example.Insert(ins3)
+	example.Insert(ins2)
+	example.Print()
+	
+if __name__ == "__main__":
+	main()
