6fe2abf85c70b49f20318a53bb5ef445d47eb44e
arvinyustin99 <arvinyustin99@gmail.com>
2019-03-30 15:14:55 +0700

    print jalur

--- Astar.py
+++ Astar.py
@@ -28,32 +28,70 @@ def Insert(ML, L):
 #input	:	matriks jalur
 #			jalur masuk dan keluar
 
-def astar(matriks, visited, Ii, Ji, If, Jf):
+def Count(L):
+	count = 0
+	for i in L:
+		count += 1
+	return count
+
+def Push(path, L):
+	t = len(path) - 1
+	if (len(path) == 0):
+		path.append(L)
+	elif (path[t][0] != L[0] or path[t][1] != L[1]):
+		path.append(L)
+
+#untuk mencari path optimal
+def Evaluate(path, If, Jf):
+	#jika elemen terakhir path bukan GOAL, buang
+	#pengecekan dari GOAL ke AWAL
+	#Jika path[i] dengan path[i-1] berselisih petak != 1, tahan di i
+	#iterasi dari j = i-1 hingga 1, bandingkan j dengan i, jika selisih petak != 1
+	#buang path[j]
+	path.reverse()
+	i = 0
+	while (i < len(path)-1):
+		if (path[0][0] == If and path[0][1] == Jf and i < len(path)-1):
+			j = i
+			while (ManhattanDist(path[j][0], path[j][1], path[j+1][0], path[j+1][1]) > 1) and (j != len(path)):
+				path.remove(path[j+1])
+				j += 1
+			i += 1
+		else:
+			path.remove(path[0])
+	path.reverse()
+
+def astar(matriks, visited, path, Ii, Ji, If, Jf):
 	#asumsi entrJ dan out sudah benar
 	steppedCost = 0
 	upperBound = len(matriks)**2
 	queue =[[Ii, Ji, steppedCost, ManhattanDist(Ii, Ji, If, Jf)]]
+	Push(path, [Ii, Ji])
 	found = False
 	finished = False
 	while (finished == False):
 		#masukkan simpul tetangga ke queue
-		if (upperBound > steppedCost and queue.count() > 0):
+		if (upperBound > steppedCost and Count(queue) > 0):
 			#MoveLeft
 			if (IsPathValid(matriks, visited, Ii, Ji - 1) == 1):
 				tempList = [Ii, Ji - 1, steppedCost, ManhattanDist(Ii, Ji - 1, If, Jf)]
 				Insert(queue, tempList)
+				Push(path, [Ii, Ji -1])
 			#MoveRight
 			if (IsPathValid(matriks, visited, Ii, Ji + 1) == 1):
 				tempList = [Ii, Ji + 1, steppedCost, ManhattanDist(Ii, Ji + 1, If, Jf)]
 				Insert(queue, tempList)
+				Push(path, [Ii, Ji+1])
 			#MoveDown
 			if (IsPathValid(matriks, visited, Ii + 1, Ji) == 1):
 				tempList = [Ii + 1, Ji, steppedCost, ManhattanDist(Ii + 1, Ji, If, Jf)]
 				Insert(queue, tempList)
+				Push(path, [Ii +1, Ji])
 			#MoveUp
 			if (IsPathValid(matriks, visited, Ii - 1, Ji) == 1):
 				tempList = [Ii - 1, Ji, steppedCost, ManhattanDist(Ii - 1, Ji, If, Jf)]
 				Insert(queue, tempList)
+				Push(path, [Ii -1, Ji])
 			#buang list sekarang
 			visited[Ii][Ji] = 7
 			steppedCost = (queue.pop(0))[2] + 1
@@ -61,19 +99,15 @@ def astar(matriks, visited, Ii, Ji, If, Jf):
 				upperBound = steppedCost
 				found = True
 
-				Ii = queue[0][0]
-				Ji = queue[0][1]
+			Ii = queue[0][0]
+			Ji = queue[0][1]
 
-		elif (upperBound <= steppedCost and queue.count() > 0):
+		elif (upperBound <= steppedCost and Count(queue)> 0):
 			visited[Ii][Ji] = 7
 			steppedCost =(queue.pop(0))[2]
 		else:
 			finished = True
 			#Periksa sisa elemen pada Queue yg F(N) < upperBound
-	while (queue.count >0):
-		costList = queue.pop(0)
-		cost = costList[2] + costList[3]
-		#if (cost < upperBound):
 
 
 def main():
@@ -102,13 +136,18 @@ def main():
 		for j in range(0, len(matriks), 1):
 			visited[i].append(0)
 
-
-	astar(matriks, visited, 1, 0, 9, 10)
+	path = []
+	astar(matriks, visited, path, 1, 0, 9, 10)
+	print(path)
+	Evaluate(path, 9, 10)
+	print(path)
+	for itr in range (0, len(path), 1):
+		matriks[path[itr][0]][path[itr][1]] = 7
 	for i in range (0, len(matriks), 1):
 		for j in range(0, len(matriks), 1):
 			if (matriks[i][j] == 1):
 				print("#", end = " ")
-			elif (visited[i][j] == 7):
+			elif (matriks[i][j] == 7):
 				print("O", end = " ")
 			else:
 				print(" ", end = " ")
